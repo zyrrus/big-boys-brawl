4518747ced58a1c51c6f1e737fa409a4e4969575
Zeke <zekeabshire@gmail.com>
2022-01-05 22:06:26 -0600

    Implemented respawn when falling off map

---
+++ Assets/Scripts/Killbox.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killbox : MonoBehaviour
+{
+    public float deathLevel;
+    private Vector2 respawnPoint;
+
+    void Start() {
+        respawnPoint = GameObject.FindGameObjectWithTag("Respawn").transform.position;
+    }
+
+    void Update() {
+        if (transform.position.y < deathLevel)
+            transform.position = respawnPoint;
+    }
+}
