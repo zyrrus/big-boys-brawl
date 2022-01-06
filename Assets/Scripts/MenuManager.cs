85a1dda5c867c92051acc8d50b33f8bef0e0366d
Zeke <zekeabshire@gmail.com>
2022-01-05 23:49:05 -0600

    Started menu system

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public static void ClickPlay() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public static void ClickQuit() {
+        Application.Quit();
+    }
+}
